4d48eeae4df8e4b6d0401f82142821e41e0b1792
aridel1997 <arinadelendik1997@gmail.com>
2021-04-09 15:57:17 +0300

    feat: add new algorithm for 03-sort-by-height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,34 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const sortedArr = arr.slice().sort((a, b) => a - b);
+  const arrResult = [];
+  let index = 0;
+  for (let i = 0; i < sortedArr.length; i++) {
+    if (sortedArr[i] === -1) {
+      sortedArr.shift();
+      i = -1;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    arrResult.push(arr[i]);
+    if (arr[i] !== -1) {
+      for (let j = 0; j < sortedArr.length; j++) {
+        if (index !== 0) {
+          arrResult[i] = sortedArr[index];
+          index++;
+          j = sortedArr.length;
+        } else {
+          arrResult[i] = sortedArr[j];
+          index = 1;
+          j = sortedArr.length;
+        }
+      }
+    }
+  }
+
+  return arrResult;
 }
 
 module.exports = sortByHeight;
