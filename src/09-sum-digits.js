9b108780d4fddfe0ebfeb44a54eb326170dea6a9
aridel1997 <arinadelendik1997@gmail.com>
2021-04-10 21:36:26 +0300

    feat: add new algorithm for rs-school-short-track-2021

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /**
  * Given a number, replace this number with
  * the sum of its digits until we get to a one digit number.
@@ -10,8 +11,25 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(number) {
+  let sum = 0;
+  let sumResult = 0;
+  let remainder = 0;
+  while (number > 0) {
+    sum += number % 10;
+    remainder = number % 10;
+    number = (number - remainder) / 10;
+  }
+  if (sum > 9) {
+    while (sum > 9) {
+      sumResult += sum % 10;
+      remainder = sum % 10;
+      sum = (sum - remainder) / 10;
+    }
+    sumResult += sum;
+    return sumResult;
+  }
+  return sum;
 }
 
 module.exports = getSumOfDigits;
