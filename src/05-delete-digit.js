7e2f17eb4b3ff317cf972bf000e50d7a2b4e22c7
aridel1997 <arinadelendik1997@gmail.com>
2021-04-11 19:44:39 +0300

    feat: add new algorithm for 05-delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,28 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(numb1) {
+  const numb = String(numb1);
+  const array = numb.split('');
+  const newArray = [];
+  let minNumb = array[0];
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (minNumb > array[i]) {
+      minNumb = array[i];
+    }
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== minNumb) {
+      newArray.push(array[i]);
+    } else if (array[i] === minNumb && count === 0) {
+      count++;
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  const resultStr = newArray.join('');
+  return +resultStr;
 }
 
 module.exports = deleteDigit;
