345e600e435dbb1ad245bf017925d20421851188
aridel1997 <arinadelendik1997@gmail.com>
2021-04-12 23:24:11 +0300

    feat: add new algorithm for 10-find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,27 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let searchIndex;
+  let arrayLength = Math.floor(array.length / 2);
+  if (array[arrayLength] > value) {
+    for (let i = 0; i < arrayLength; i++) {
+      arrayLength = Math.floor(arrayLength / 2);
+      if (array[i] === value) {
+        searchIndex = i;
+      }
+    }
+  } else {
+    for (let i = arrayLength; i < array.length; i++) {
+      if (array[i] === value) {
+        searchIndex = i;
+      }
+    }
+  }
+  if (typeof searchIndex === 'number') {
+    return searchIndex;
+  }
+  return false;
 }
 
 module.exports = findIndex;
