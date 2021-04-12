d93007ac969c104f392a038089939288d99862ca
aridel1997 <arinadelendik1997@gmail.com>
2021-04-12 19:09:15 +0300

    feat: add new algorithm for 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const array = str.split('');
+  const arrayResult = [];
+  let counter = 1;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === array[i + 1]) {
+      counter++;
+    } else if (counter === 1) {
+      arrayResult.push(array[i]);
+    } else {
+      arrayResult.push(`${counter}${array[i]}`);
+      counter = 1;
+    }
+  }
+  const strResult = arrayResult.join('');
+  return strResult;
 }
 
 module.exports = encodeLine;
